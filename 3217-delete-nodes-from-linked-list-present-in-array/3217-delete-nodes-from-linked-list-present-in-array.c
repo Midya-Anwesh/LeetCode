5b5cd1db80d9c621874e198e40435902fb0c961c
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-09-06 20:41:16 +0530

    Time: 303 ms (21.25%), Space: 129.6 MB (7.5%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.c
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.c
@@ -6,31 +6,36 @@
  * };
  */
 struct ListNode* modifiedList(int* nums, int numsSize, struct ListNode* head) {
+    // creating hash table
     int *table = (int *)calloc(sizeof(int), 100001);
     for (int i = 0; i < numsSize; table[nums[i++]]++);
 
+    // defining pointers to travrese on ll
     struct ListNode *pre = NULL, *curr = head;
 
     while (curr != NULL){
+        // if we have to delete a node
         if ( table[(curr -> val)] ){
+            // if head is to be deleted
             if (curr == head){
                 pre = head;
                 head = head -> next;
                 curr = head;
             }
+            // if any other node is to be deleted
             else{
                 pre -> next = curr -> next;
                 free(curr);
                 curr = pre -> next;
             }
         }
-
+        // if current node is not be deleted
         else {
             pre = curr;
             curr = curr -> next;           
         }
     }
-
+    // free the hash table
     free(table);
 
     return head;
