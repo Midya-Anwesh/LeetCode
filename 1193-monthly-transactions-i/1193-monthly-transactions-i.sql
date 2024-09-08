99027a79e48720eac33a2c6795c7d757d534481c
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-09-08 22:15:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+SELECT derived1.month, derived1.country, trans_count, IFNULL(approved_count, 0) AS approved_count, 
+trans_total_amount, IFNULL(approved_total_amount, 0) AS approved_total_amount
+FROM
+
+    (
+        SELECT id, DATE_FORMAT(trans_date, '%Y-%m') AS month, country, COUNT(country) AS trans_count, 
+        SUM(amount) AS trans_total_amount FROM Transactions t1
+        GROUP BY country, month
+    ) derived1
+
+    LEFT JOIN
+
+    (
+        SELECT id, country,DATE_FORMAT(trans_date, '%Y-%m') AS month, SUM(amount) AS approved_total_amount,
+        COUNT(country) AS approved_count FROM Transactions
+        WHERE state="approved"
+        GROUP BY country, month
+    ) derived2
+
+    ON derived1.country = derived2.country AND derived1.month = derived2.month;
