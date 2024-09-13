b7fc69d5a1613b9d93405ae33ba378865508ca15
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-09-13 20:50:37 +0530

    Time: 105 ms (22.45%), Space: 19.4 MB (51.59%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -18,26 +18,34 @@ ListNode.__lt__ = __lt__
 from heapq import heapify, heappush, heappop
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        # ret is newly created linked list and heap is min-heap
         heap, ret = [], None
+
+        # temp is to track the last node of newly created linked list
         temp = None
         heapify(heap)
+
         # Add head of all linked lists in min-heap except null
         for head in filter(lambda h: not h is None, lists):
             heappush(heap, head)
 
+        # exhaust all heads of linked lists that are not null
         while len(heap):
             head = heappop(heap)
-            # if head is None:
-            #     continue
+            # if head of linked list to be returned is null then assign it to head
+            # and update the temp
             if ret is None:
                 ret = head
                 temp = ret
+            # add head after the temp and update the position of temp
             else:
                 temp.next = head
                 temp = temp.next
             head = head.next
             temp.next = None
+            # if there is a node adjecent to current head then add it to heap
             if not head is None:
                 heappush(heap, head)
-        
+        # return the head of newly created linked list
         return ret
