9dd498fa14414f32d3ec86dec6abbd3721ed26b3
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-09-17 10:28:13 +0530

    Time: 322 ms (65.73%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,11 +1,7 @@
 # Write your MySQL query statement below
-SELECT (CASE WHEN id < (SELECT COUNT(id) FROM Seat) THEN id+1
-        WHEN id >= (SELECT COUNT(id) FROM Seat) THEN id
-        END) AS id, student FROM Seat
-        WHERE id % 2
-
-UNION
-SELECT id-1 AS id, student FROM Seat
-WHERE id%2=0
-
-ORDER BY id
+SELECT (CASE WHEN id%2 AND id < (SELECT COUNT(id) FROM Seat) THEN id+1
+        WHEN id%2 AND id >= (SELECT COUNT(id) FROM Seat) THEN id 
+        WHEN id%2=0 THEN id-1
+        END)
+        AS id, student FROM Seat
+ORDER BY id;
