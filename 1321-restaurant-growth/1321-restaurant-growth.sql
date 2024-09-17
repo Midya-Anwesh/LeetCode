3307a71ae96d51c2d1da283243022a94a7139597
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-09-17 12:41:18 +0530

    Time: 335 ms (64.77%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,10 +1,8 @@
 # Write your MySQL query statement below
-
-SELECT DISTINCT(visited_on) AS visited_on, (
-    SELECT SUM(amount) FROM Customer c1
-    WHERE c1.visited_on <= c2.visited_on AND DATEDIFF(c2.visited_on, c1.visited_on) <= 6
-) AS amount, (
-    SELECT ROUND(SUM(amount)/7, 2) FROM Customer c1
-    WHERE c1.visited_on <= c2.visited_on AND DATEDIFF(c2.visited_on, c1.visited_on) <= 6
-) AS average_amount FROM Customer c2
-WHERE visited_on >= (SELECT MIN(visited_on) FROM Customer)+6
+SELECT *, ROUND(amount/7, 2) AS average_amount FROM (
+    SELECT DISTINCT(visited_on) AS visited_on, (
+        SELECT SUM(amount) FROM Customer c1
+        WHERE c1.visited_on <= c2.visited_on AND DATEDIFF(c2.visited_on, c1.visited_on) <= 6
+    ) AS amount FROM Customer c2
+    WHERE visited_on >= (SELECT MIN(visited_on) FROM Customer)+6
+) AS derived1
