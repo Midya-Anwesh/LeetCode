43883de6cefd9b0d460688ec4da4a0557bc394cb
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-10-03 11:35:25 +0530

    Time: 49 ms (18.65%), Space: 16.7 MB (17.33%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,16 +1,14 @@
 class Solution:
-    def recurse(self, n: int, curr: int, encoding: str) -> str:
-        if curr >= n:
-            return encoding
-        count, ch, new_encoding = 1, encoding[0], ""
-        for i in range(1, len(encoding)):
-            if ch == encoding[i]:
-                count += 1
-            else:
-                new_encoding += str(count)+ch
-                count, ch = 1, encoding[i]
-        new_encoding += str(count)+ch
-        return self.recurse(n, curr+1, new_encoding)
-
     def countAndSay(self, n: int) -> str:
-        return self.recurse(n, 1, "1")
+        old, new = "1", ""
+        for i in range(n-1):
+            count, ch = 1, old[0]
+            for i in range(1, len(old)):
+                if ch == old[i]:
+                    count += 1
+                else:
+                    new += str(count)+ch
+                    count, ch = 1, old[i]
+            new += str(count)+ch
+            old, new = new, ""
+        return old
