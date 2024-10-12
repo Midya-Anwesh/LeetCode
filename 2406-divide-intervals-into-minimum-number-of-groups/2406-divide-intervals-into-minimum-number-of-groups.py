6eb2b75a1b6b860fd6fe428a94b0597a221e50ca
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-10-13 00:01:56 +0530

    Time: 1199 ms (31.01%), Space: 56.7 MB (26.16%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
@@ -1,33 +1,14 @@
-class TreeNode:
-    def __init__(self, interval: List[List[int]]) -> None:
-        self.interval = interval
-        self.left = None
-        self.right = None
-    def overlap(self, interval: List[List[int]]) -> bool:
-        return (self.interval[0] <= interval[0] <= self.interval[1]) or \
-        (self.interval[0] <= interval[1] <= self.interval[1])
-
+from heapq import heapify, heappush, heappop
 class Solution:
-    def addInterval(self, root: TreeNode, interval: List[List[int]]) -> int:
-        while (root.overlap(interval)):
-            if interval[0] <= root.interval[0]:
-                if root.left is None:
-                    root.left = TreeNode(interval)
-                    return 1
-                else:
-                    root = root.left
-            else:
-                if root.right is None:
-                    root.right = TreeNode(interval)
-                    return 1
-                else:
-                    root = root.right
-        root.interval = interval
-        return 0
-
     def minGroups(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key=lambda x:x[0])
-        ans, root = 1, TreeNode(intervals[0])
+        intervals.sort()
+        heap = [intervals[0][1]]
+        heapify(heap)
+        ans = 1
         for i in range(1, len(intervals)):
-            ans += self.addInterval(root, intervals[i])
+            prev_time = heappop(heap)
+            if prev_time >= intervals[i][0]:
+                ans += 1
+                heappush(heap, prev_time)
+            heappush(heap, intervals[i][1])
         return ans
