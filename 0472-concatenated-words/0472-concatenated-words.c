e03a0cda4677007661d952894d4bb53503bb8b6f
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-10-13 20:00:53 +0530

    Time: 146 ms (33.33%), Space: 210 MB (33.33%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.c
@@ -0,0 +1,82 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+typedef struct Trie{
+    struct Trie *child[26];
+    int wordEnd;
+}TrieNode;
+
+void init(TrieNode *root){
+    for (int i = 0; i < 26; i++){
+        root->child[i] = NULL;
+    }
+    root->wordEnd = 0;
+}
+
+int concated(TrieNode *root, char *s, int index, int len){
+    if (root == NULL){
+        return 0;
+    }
+    TrieNode *temp = root;
+    while (index < len){
+        if (temp -> child[s[index]-97] == NULL){
+            return 0;
+        }
+        temp = temp->child[s[index]-97];
+        index += 1;
+        if (temp -> wordEnd){
+            if (index >= len){
+                return 1;
+            }
+            if (concated(root, s, index, len)){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+void addWord(TrieNode **root, char *s, int len){
+    if (*root == NULL){
+        (*root) = calloc(sizeof(TrieNode), 1);
+    }
+    TrieNode *temp = *root;
+    for (int i = 0; i < len; i++){
+        if (temp->child[s[i]-97] == NULL){
+            temp->child[s[i]-97] = calloc(sizeof(TrieNode), 1);
+            // init(temp->child[s[i]-97]);
+        }
+        temp = temp->child[s[i]-97];
+    }
+    temp->wordEnd = 1;
+}
+
+int compare(const void *one, const void *two) {
+    const char *iOne = *(const char * const *)one;
+    const char *iTwo = *(const char * const *)two;
+    size_t len1 = strlen(iOne);
+    size_t len2 = strlen(iTwo);
+    if (len1 > len2)
+        return 1;
+    if (len2 > len1)
+        return -1;
+    return strcmp(iOne, iTwo);
+}
+
+char** findAllConcatenatedWordsInADict(char** words, int wordsSize, int* returnSize) {
+    qsort(words, wordsSize, sizeof(char*), compare);
+    char **ret = malloc(sizeof(char*) * wordsSize);
+    *returnSize = 0;
+    TrieNode *root = NULL;
+    for (int i = 0; i < wordsSize; i++){
+        int len = strlen(words[i]);
+        if (concated(root, words[i], 0, len)){
+            ret[(*returnSize)++] = words[i];
+        }
+        else{
+            addWord(&root, words[i], len);
+        }
+    }
+    return ret;
+}
