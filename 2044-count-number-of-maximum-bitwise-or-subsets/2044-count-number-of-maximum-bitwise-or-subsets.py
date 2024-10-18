0cce0bedd4ed9fb46a07824c830e39c1e671eaf5
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-10-18 17:49:49 +0530

    Time: 1139 ms (24.06%), Space: 17 MB (29.66%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -0,0 +1,30 @@
+from functools import reduce
+
+class Solution:
+
+    def count(self, nums: list[int], temp: list[int], index: int):
+
+        if len(temp) and len(temp) <= len(nums):
+
+            val = reduce(lambda i, j=1: i|j, temp)
+
+            self.or_dict[val] = self.or_dict.get(val, 0)+1
+
+            
+
+        for i in range (index, len(nums)):
+
+            temp.append(nums[i])
+
+            self.count(nums, temp, i+1)
+
+            temp.pop(-1)
+
+    def countMaxOrSubsets(self, nums: list[int]) -> int:
+
+      self.or_dict = dict()
+
+      self.count(nums, [], 0)
+
+      return self.or_dict[max(self.or_dict)]
+
