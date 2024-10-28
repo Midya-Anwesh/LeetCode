e0def6209d0a404d2f02d4cdf932e24a3e8326c2
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-10-28 09:43:42 +0530

    Time: 171 ms (16.22%), Space: 53.1 MB (5.33%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.py
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.py
@@ -3,24 +3,19 @@ class Solution:
         nums = {num:1 for num in nums}
         ret = -1
 
-        def bfs(num: int) -> int:
-            length_of_streak = 0
+        def dfs(num: int) -> int:
+            if not num in nums:
+                return 0
+            if not nums[num]:
+                return 0
             nums[num] = 0
-            queue = [num]
-            while len(queue):
-                length_of_streak += 1
-                curr = queue.pop(0)
-                if ((curr_sqrt:=curr**(1/2)) in nums) and nums[curr_sqrt]:
-                    queue.append(curr_sqrt)
-                    nums[curr_sqrt] = 0
-                if ((curr_sqr:=curr*curr) in nums) and nums[curr_sqr]:
-                    queue.append(curr_sqr)
-                    nums[curr_sqr] = 0
-            return length_of_streak
+            left_len = dfs(num**(1/2))
+            right_len = dfs(num*num)
+            return left_len + right_len + 1
 
         for num in nums:
             if nums[num]:
-                len_of_longest = -1 if (l:=bfs(num)) <= 1 else l
+                len_of_longest = -1 if (l:=dfs(num)) <= 1 else l
                 ret = max(ret, len_of_longest)
 
         return ret
