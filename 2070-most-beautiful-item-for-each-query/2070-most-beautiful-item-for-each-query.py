60b3ed34823c91d87da8f8240b5a3f719936dc4f
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-11-12 14:08:22 +0530

    Time: 458 ms (6.1%), Space: 66.5 MB (60.22%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
@@ -19,19 +19,14 @@ class Solution:
         # Get max bueaty possible for a certain price
         items.sort() # Sorting, to make the next step (normalization) easy
         priceToBeauty = dict()
+        prevMaxBeauty = -float('inf')
         for item in items:
             price, beauty = item
-            priceToBeauty[price] = max(priceToBeauty.get(price, -float('inf')), beauty)
-
-        # Normalize results, if less price gets more beauty then change the beauty of current price
-        prevMaxBeauty = -float('inf')
-        for price in priceToBeauty:
-            priceToBeauty[price] = max(prevMaxBeauty, priceToBeauty[price])
-            prevMaxBeauty = max(prevMaxBeauty, priceToBeauty[price])
+            priceToBeauty[price] = max(priceToBeauty.get(price, -float('inf')), beauty, prevMaxBeauty)
+            prevMaxBeauty = max(prevMaxBeauty, priceToBeauty[price])            
 
         ret = []
         for query in queries:
             # Append maximum beauty possible in current price if not then 0
             ret.append(priceToBeauty.get(findAbsoluteOrClosest(items, query), 0))
-        return ret
-        
+        return ret        
