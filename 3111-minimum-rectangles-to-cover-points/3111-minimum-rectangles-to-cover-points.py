140f67fdd7471fee27b8c855f9a6a4c42eba385d
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-11-23 06:43:17 +0530

    Time: 47 ms (12.84%), Space: 54.8 MB (53.05%) - LeetHub

--- 3111-minimum-rectangles-to-cover-points/3111-minimum-rectangles-to-cover-points.py
+++ 3111-minimum-rectangles-to-cover-points/3111-minimum-rectangles-to-cover-points.py
@@ -1,8 +1,8 @@
 class Solution:
     def minRectanglesToCoverPoints(self, points: List[List[int]], w: int) -> int:
         points.sort(key=lambda pos:(pos[0], pos[1]))
-        lower_ends = [points[0][0]]
+        ret, last_lowest_point = 1, points[0][0]
         for i in range(1, len(points)):
-            if points[i][0] - lower_ends[-1] > w:
-                lower_ends.append(points[i][0])
-        return len(lower_ends)
+            if points[i][0] - last_lowest_point > w:
+                ret, last_lowest_point = ret+1, points[i][0]
+        return ret
