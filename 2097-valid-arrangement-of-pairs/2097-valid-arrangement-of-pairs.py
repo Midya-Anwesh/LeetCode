3a6b715e5c67ea897aa31e5db758b0066523dc7a
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-11-30 18:02:24 +0530

    Time: 620 ms (5.43%), Space: 159.1 MB (5.54%) - LeetHub

---
+++ 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
+        # in_out_count to count number of ingoing and outgoing edges
+        # vertex: [ingoing count, outgoing count]
+
+        # connected_edges to track which edges are connected to which node
+        # vertex: [ [vertex, destination] ]
+        m = max(pairs, key=lambda edge:max(edge))
+        in_out_count, connected_edges = defaultdict(lambda:[0,0]), defaultdict(lambda:deque([]))
+
+        for edge in pairs:
+            connected_edges[edge[0]].append(edge)
+            in_out_count[edge[0]][1] += 1
+            in_out_count[edge[1]][0] += 1
+        
+        # As we are told testcases are designed such that they have euler path
+        start = pairs[0][0] # We consider first node as start point
+
+        for vertex in in_out_count:
+            # If any node has one more outgoing edge than incoming then that will be start node
+            if in_out_count[vertex][1] - in_out_count[vertex][0] == 1:
+                start = vertex
+
+        ret = []
+        def hierholzer(vertex: int, prev: None|int) -> None:
+            while len(connected_edges[vertex]):
+                edge = connected_edges[vertex].popleft()
+                hierholzer(edge[1], vertex)
+            if prev is not None:
+                ret.append([prev, vertex])
+        hierholzer(start, None)
+        return ret[::-1]
