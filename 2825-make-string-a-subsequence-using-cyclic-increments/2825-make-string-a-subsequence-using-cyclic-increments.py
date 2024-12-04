8838880ce010c4f179be266cef60c72cb71efc50
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2024-12-04 14:11:01 +0530

    Time: 94 ms (28.47%), Space: 17.7 MB (31.28%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
@@ -1,38 +1,18 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        s1, s2 = dict(), dict()
-        for char in str1:
-            s1[char] = s1.get(char, 0)+1
-        for char in str2:
-            s2[char] = s2.get(char, 0)+1
-
-        for char in s2:
-            while s2[char]:
-                if s1.get(char, 0) > 0:
-                    next_alpha = chr(((ord(char)-96)%26)+97)
-                    prev_alpha = chr((((ord(char)-97)+25)%26)+97)
-                    if s2.get(next_alpha, 0):
-                        if s1[char]+s1.get(next_alpha, 0) < s2[next_alpha]:
-                            return False
-                        elif s1[char]+s1.get(next_alpha, 0) == s2[next_alpha]:
-                            if s1.get(prev_alpha, 0):
-                                s1[prev_alpha] -= 1
-                                # s2[char] -= 1
-                            else:
-                                return False
-                        else:
-                            s1[char] -= 1
-                            # s2[char] -= 1
-                    else:
-                        s1[char] -= 1
-
+        s1, s2 = 0, 0
+        while s2 < len(str2) and s1 < len(str1):
+            if str1[s1] == str2[s2]:
+                s1 += 1
+                s2 += 1
+            else:
+                next_alpha = chr(((ord(str1[s1])-97+1)%26)+97)
+                if next_alpha == str2[s2]:
+                    s1 += 1
+                    s2 += 1
                 else:
-                    prev_alpha = chr((((ord(char)-97)+25)%26)+97)
-                    if s1.get(prev_alpha, 0):
-                        s1[prev_alpha] -= 1
-                    else:
-                        return False
-                
-                s2[char] -= 1
-
-        return True
+                    s1 += 1
+        
+        if s2 >= len(str2):
+            return True
+        return False
