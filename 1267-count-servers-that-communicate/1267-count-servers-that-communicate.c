9f03a344aa44dc83ca9fc2a461df11383f205829
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2025-01-23 10:36:31 +0530

    Time: 2 ms (41.67%), Space: 15.7 MB (0%) - LeetHub

---
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.c
@@ -0,0 +1,49 @@
+int countServers(int** grid, int gridSize, int* gridColSize) {
+    int **seen = (int**)calloc(sizeof(int*), gridSize);
+    for (int i = 0; i < gridSize; i++){
+        seen[i] = (int*)calloc(sizeof(int), (*gridColSize));
+    }
+    int connected = 0;
+
+    for (int i = 0; i < gridSize; i++){
+        int frow = 0, fcol = 0, comAll = 0, comSeen = 0;
+        for (int j = 0; j < (*gridColSize); j++){
+            if (grid[i][j]){
+                if (!comAll){
+                    frow = i; fcol = j;
+                }
+                comAll += 1;
+                comSeen += ((seen[i][j] >= 1)? 1:0);
+                seen[i][j] += 1;
+            }
+        }
+        if (comAll == 1){
+            seen[frow][fcol] -= 1;
+        }
+        else if (comAll > 1){
+            connected += comAll-comSeen;
+        }
+    }
+
+    for (int j = 0; j < (*gridColSize); j++){
+        int frow = 0, fcol = 0, comAll = 0, comSeen = 0;
+        for (int i = 0; i < gridSize; i++){
+            if (grid[i][j]){
+                if (!comAll){
+                    frow = i; fcol = j;
+                }
+                comAll += 1;
+                comSeen += ((seen[i][j] >= 1) ? 1:0);
+                seen[i][j] += 1;
+            }
+        }
+        if (comAll == 1){
+            seen[frow][fcol] -= 1;
+        }
+        else if (comAll > 1){
+            connected += comAll-comSeen;
+        }
+    }
+
+    return connected;
+}
