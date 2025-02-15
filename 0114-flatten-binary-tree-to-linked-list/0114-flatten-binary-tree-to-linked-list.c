3ab3f455e293f9e48a093f3dba51ba956bd46960
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2025-02-15 15:29:44 +0530

    Time: 0 ms (100%), Space: 11.2 MB (72.84%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.c
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.c
@@ -7,18 +7,24 @@
  * };
  */
 void flatten(struct TreeNode* root) {
-    if (!root){
+    if (!root){ // If root is NULL return
         return;
     }
+    // Recursivly flatten the left subtree
     flatten(root -> left);
+    // If we get parent of a leaf node, save pointer to it's right subtree in a verable(temp)
+    // Add it's left child to the right pointer, make the left subtree NULL
     struct TreeNode* temp = root -> right;
     root -> right = root -> left;
     root -> left = NULL;
+    // Recursively flatten the right sub-tree
     flatten(temp);
+    // If there is no right child then add the right-sub tree to the right pointer
     if (!(root -> right)){
         root -> right = temp;
         return;
     }
+    // Otherwise find the leaf node of right sub tree and add flattened sub tree to it's right
     struct TreeNode* t2 = root -> right;
     while (t2 && (t2 -> right)){
         t2 = t2 -> right;
