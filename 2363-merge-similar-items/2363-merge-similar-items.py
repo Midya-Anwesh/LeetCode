b4f5fd94d4f1a7b5b08b8d5e657e8587fac8f06f
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2025-03-02 10:13:34 +0530

    Time: 4 ms (58.75%), Space: 18.5 MB (78.49%) - LeetHub

---
+++ 2363-merge-similar-items/2363-merge-similar-items.py
@@ -0,0 +1,29 @@
+class Solution:
+    def mergeSimilarItems(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        nums1.sort(key=lambda nums:nums[0])
+        nums2.sort(key=lambda nums:nums[0])
+        num1Index, num2Index = 0, 0
+        ret = []
+
+        while (num1Index < len(nums1)) and (num2Index < len(nums2)):
+            if nums1[num1Index][0] == nums2[num2Index][0]:
+                ret.append([ nums1[num1Index][0], nums1[num1Index][1] + nums2[num2Index][1] ])
+                num1Index += 1
+                num2Index += 1
+
+            elif nums1[num1Index][0] < nums2[num2Index][0]:
+                ret.append(nums1[num1Index])
+                num1Index += 1
+
+            else:
+                ret.append(nums2[num2Index])
+                num2Index +=1
+        
+        while num1Index < len(nums1):
+            ret.append(nums1[num1Index])
+            num1Index += 1
+        while num2Index < len(nums2):
+            ret.append(nums2[num2Index])
+            num2Index += 1
+        
+        return ret
