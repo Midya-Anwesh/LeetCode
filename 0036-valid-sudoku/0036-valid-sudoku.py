c73abcc69d90c8e461a2d8059171b3d47c03bc42
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2025-08-30 23:14:43 +0530

    Time: 3 ms (81.81%), Space: 18 MB (24.98%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -11,18 +11,15 @@ class Solution:
                 if board[i][j] != ".":
                     boxNo = ((len(board[0]) // 3) * (i // 3)) + (j // 3)
                     if board[i][j] in subBoxes[boxNo]:
-                        print(f"pos = {i, j} boxNo = {boxNo} | {board[i][j]} repeated in sub-box {boxNo}")
                         return False
                     else:
                         subBoxes[boxNo].add(board[i][j])
                     if board[i][j] in rowNums:
-                        print(f"{board[i][j]} repeated in row {i}")
                         return False
                     else:
                         rowNums.add(board[i][j])
                 if board[j][i] != ".":
                     if board[j][i] in colNums:
-                        print(f"{board[j][i]} repeated in col {i}")
                         return False
                     else:
                         colNums.add(board[j][i])
