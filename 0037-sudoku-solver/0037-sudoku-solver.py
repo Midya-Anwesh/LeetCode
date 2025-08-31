bdbfea009869564c059c6e8d9e0293c58db9e392
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2025-08-31 12:17:01 +0530

    Time: 567 ms (84.53%), Space: 18.1 MB (21.32%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,31 +1,26 @@
 from collections import defaultdict
+UNIVERSE = set([chr(i) for i in range(49, 58)])
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        # First cache all pre-placed nums in rows, cols and sub-boxes for quick processing
+        # First compute which digits are safe to place along a row, or column or, sub-box
         # And also, check which cells needs to be filled
-        rowNums, colNums, subBoxNums = defaultdict(set), defaultdict(set), defaultdict(set)
+        rowNums, colNums, subBoxNums = defaultdict(lambda:UNIVERSE.copy()), defaultdict(lambda:UNIVERSE.copy()), defaultdict(lambda:UNIVERSE.copy())
         cells = []
         for i in range(len(board)):
             for j in range(len(board)):
                 if board[i][j] != ".":
-                    rowNums[i].add(board[i][j])
+                    rowNums[i].discard(board[i][j])
                     subBoxNo = (3 * (i // 3)) + (j // 3)
-                    subBoxNums[subBoxNo].add(board[i][j])
+                    subBoxNums[subBoxNo].discard(board[i][j])
                 else:
                     cells.append((i, j))
 
                 if board[j][i] != ".":
-                    colNums[i].add(board[j][i])
-        
-        # Functions to find if placing a number to a specific cell is safe
-        def isSafe(row: int, col: int, char: str) -> bool:
-            subBoxNo = (3 * (row //3)) + (col // 3)
-            return (char not in rowNums[row]) and (char not in colNums[col]) and (char not in subBoxNums[subBoxNo])
-        
-        # Flag to mark if sudoku is solved or not
+                    colNums[i].discard(board[j][i])
+
         solved = False
 
         # Recursive function to place digits 1-9 in vacant places to solve sudoku
@@ -35,21 +30,18 @@ class Solution:
                 solved = True
                 return
             row, col = cells[idx]
-            for i in range(49, 58):
-                char = chr(i)
-                if isSafe(row, col, char):
-                    subBoxNo = (3 * (row //3)) + (col // 3)
-                    board[row][col] = char
+            subBoxNo = (3 * (row //3)) + (col // 3)
+            for char in rowNums[row].intersection(colNums[col]).intersection(subBoxNums[subBoxNo]):
+                rowNums[row].discard(char)
+                colNums[col].discard(char)
+                subBoxNums[subBoxNo].discard(char)
+                placeDigits(idx+1)
+                if not solved:
                     rowNums[row].add(char)
                     colNums[col].add(char)
                     subBoxNums[subBoxNo].add(char)
-                    placeDigits(idx+1)
-                    if not solved:
-                        board[row][col] = "."
-                        rowNums[row].discard(char)
-                        colNums[col].discard(char)
-                        subBoxNums[subBoxNo].discard(char)
-                    else:
-                        break
-        
+                else:
+                    board[row][col] = char
+                    return
+
         placeDigits(0)
