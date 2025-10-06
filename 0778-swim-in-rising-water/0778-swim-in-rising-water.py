930ab9ec11b33c1dc486cb1304b34d5c9fab8e8d
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2025-10-06 11:54:25 +0530

    Time: 35 ms (45.9%), Space: 18.4 MB (53.12%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,21 +1,23 @@
-from heapq import heapify, heappop, heappush
-ADJ_CELLS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+from heapq import heappush, heappop
+
+ADJ = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        costs = {(i, j): float('inf') for i in range(len(grid)) for j in range(len(grid[0]))}
-        costs[(0, 0)] = grid[0][0]
-
-        def getMin(row: int, col: int) -> None:
-            if row == len(grid)-1 and col == len(grid[0])-1:
-                return
-            heap = [(costs[(row, col)], row, col)]
-            heapify(heap)
+        def dijkstra(row: int, col: int) -> int:
+            minTime = {(i, j): float('inf') for i in range(len(grid)) for j in range(len(grid[0]))}
+            minTime[(row, col)] = grid[row][col]
+            heap = [(grid[row][col], row, col)]
             while len(heap):
-                cost, row, col = heappop(heap)
-                for dx, dy in ADJ_CELLS:
-                    if (row+dx, col+dy) in costs:
-                        if max(cost, grid[row+dx][col+dy]) < costs[(row+dx, col+dy)]:
-                            costs[(row+dx, col+dy)] = max(cost, grid[row+dx][col+dy])
-                            heappush(heap, [costs[(row+dx, col+dy)], row+dx, col+dy])
-        getMin(0, 0)
-        return costs[(len(grid)-1, len(grid[0])-1)]
+                currTime, r, c = heappop(heap)
+                if (r == len(grid)-1) and (c == len(grid[0])-1):
+                    break
+                for dx, dy in ADJ:
+                    nr, nc = r+dx, c+dy
+                    if nr < 0 or nr >= len(grid) or nc < 0 or nc >= len(grid[0]):
+                        continue
+                    timeToNextNode = max(currTime, grid[nr][nc])
+                    if timeToNextNode < minTime[(nr, nc)]:
+                        minTime[(nr, nc)] = timeToNextNode
+                        heappush(heap, (timeToNextNode, nr, nc))
+            return minTime[(len(grid)-1, len(grid[0])-1)]
+        return dijkstra(0, 0)
