22612d113772023fd33a67321641417cb4169c76
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2025-11-22 00:38:38 +0530

    Time: 642 ms (36.57%), Space: 49.5 MB (5.11%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -1,31 +1,23 @@
 from collections import defaultdict
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        # Hash map to store first and last occurence of an alphabet
-        indexMap = defaultdict(lambda : [float('inf'), -float('inf')])
-
-        # Fill values in hash map
+        charFreq = [0 for _ in range(26)]
+        prefix = [[0 for _ in range(26)]]
+        idxMap = defaultdict(lambda:[float('inf'), -float('inf')])
         for idx, char in enumerate(s):
-            indexMap[char][0] = min(idx, indexMap[char][0])
-            indexMap[char][1] = max(idx, indexMap[char][1])
+            idxMap[char][0] = min(idxMap[char][0], idx)
+            idxMap[char][1] = max(idxMap[char][1], idx)
+            prefix.append(prefix[-1].copy())
+            prefix[-1][ord(char)-97] += 1
 
-        # Return variable
-        ans = 0
-
-        # Traverse alphabets in the map
-        for char in indexMap:
-
-            # Use a set to keep track of distinct alphabets
-            seen = set()
-
-            # Traverse the string from first occurence index + 1 to last occurence index - 1
-            for i in range(indexMap[char][0]+1, indexMap[char][1]):
-                # Add alphabet to set
-                seen.add(s[i])
-            # Length of the set will be number of distinct alphabets between first occurence and last occurence index
-            # Which is number of distinct/unique length 3 pallindromic sub-sequence that can be formed using that alphabet
-            # So, we add it to return variable
-            ans += len(seen)
-
-        # Finally return number of unique palindromes of length three
-        return ans
+        pallindromes = 0
+        charsSeen = set()
+        for char in s:
+            if char in charsSeen:
+                continue
+            charsSeen.add(char)
+            leftIdx, rightIdx = idxMap[char]
+            currSet = [prefix[rightIdx][i] - prefix[leftIdx][i] for i in range(26)]
+            currSet[ord(char)-97] -= 1
+            pallindromes += sum(freq >= 1 for freq in currSet)
+        return pallindromes
