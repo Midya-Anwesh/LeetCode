9aa7731ebdb6ed59e6eb1e3e0138914394342566
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2025-12-09 20:51:08 +0530

    Time: 732 ms (60.36%), Space: 52 MB (5.4%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.py
+++ 3583-count-special-triplets/3583-count-special-triplets.py
@@ -1,6 +1,17 @@
 from collections import defaultdict
 from bisect import bisect_left
 class Solution:
+    """
+    To solve it, first we need to understand,
+    No of triplates can be formed is (num*2 present in the left to current num) * (num*2 present in the right of current num)
+
+    To achive this,
+    We will traverse the list and store the indices to list of respective num using a hash table
+    As we are traversing left to right, indices are already sorted
+
+    So, After filling the table we can use binary search to find how many numbers are in left and right
+    In case of 0 we need to substract 1 from elements present in right, as 2*0 = 0, so it resides in the same list
+    """
     def specialTriplets(self, nums: List[int]) -> int:
         indices = defaultdict(list)
         for idx, num in enumerate(nums):
