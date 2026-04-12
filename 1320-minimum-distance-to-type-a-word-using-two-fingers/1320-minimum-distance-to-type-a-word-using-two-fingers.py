818ae1bbc0c26af2039b1cf306d0c684a4f01f96
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2026-04-12 23:06:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.py
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.py
@@ -3,10 +3,10 @@ from functools import lru_cache
 class Solution:
     def minimumDistance(self, word: str) -> int:
         # Function to calculate cost of typing current letter, based on previous letter typed
-        def getCost(prev: str | -1, curr: str) -> int:
+        def getCost(prev: int, curr: int) -> int:
             if prev == -1:
                 return 0
-            prevAscii, currAscii = ord(prev)-65, ord(curr)-65
+            prevAscii, currAscii = ord(word[prev])-65, ord(word[curr])-65
             x1, y1 = prevAscii // 6, prevAscii % 6
             x2, y2 = currAscii // 6, currAscii % 6
             return abs(x1 - x2) + abs(y1- y2)
@@ -19,9 +19,9 @@ class Solution:
             # Return minimum of two possibilities
             return min(
                 # Try typing using left finger
-                dfs(idx+1, idx, right) + getCost(-1 if left == -1 else word[left], word[idx]),
+                dfs(idx+1, idx, right) + getCost(left, idx),
                 # Try typing using right finger
-                dfs(idx+1, left, idx) + getCost(-1 if right == -1 else word[right], word[idx])
+                dfs(idx+1, left, idx) + getCost(right, idx)
             )
         
         return dfs(0, -1, -1)
