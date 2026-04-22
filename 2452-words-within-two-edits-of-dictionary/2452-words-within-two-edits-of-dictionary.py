697b63dd05bcc796ae04a5ff17c81bd8f00cb009
Midya-Anwesh <140254341+Midya-Anwesh@users.noreply.github.com>
2026-04-22 22:58:03 +0530

    Time: 87 ms (43.14%), Space: 23.8 MB (5.88%) - LeetHub

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -0,0 +1,68 @@
+class TrieNode:
+    def __init__(self):
+        self.childs = [None for _ in range(26)]
+        self.wordEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, word: str) -> None:
+        if self.root is None:
+            self.root = TrieNode()
+        node = self.root
+        idx = 0
+        while idx < len(word):
+            curr = ord(word[idx]) - 97
+            if node.childs[curr] is None:
+                node.childs[curr] = TrieNode()
+            node = node.childs[curr]
+            idx += 1
+            if idx >= len(word):
+                node.wordEnd = True
+    
+    def find(self, editsAllowed: int, word: str) -> bool:
+        if self.root is None:
+            return False
+        node = self.root
+
+        def dfs(node: Optional[TrieNode], edits: int, idx: int) -> bool:
+            curr = ord(word[idx]) - 97
+            # If it's the last letter
+            if idx == len(word)-1:
+                # If we have edits available just return true
+                if edits > 0:
+                    return True
+                # Othwise return if it's a word end
+                if node.childs[curr] is not None:
+                    return node.childs[curr].wordEnd
+                return False
+
+            # Traverse for current letter in trie
+            found = False
+            if node.childs[curr] is not None:
+                found |= dfs(node.childs[curr], edits, idx+1)
+
+            # If not found try with edits
+            if (not found) and (edits > 0):
+                for i in range(26):
+                    if node.childs[i] is None:
+                        continue
+                    found |= dfs(node.childs[i], edits-1, idx+1)
+            
+            # Return the result
+            return found
+        
+        return dfs(node, editsAllowed, 0)
+
+
+class Solution:
+    def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
+        trie = Trie()
+        for word in dictionary:
+            trie.insert(word)
+        ret = []
+        for word in queries:
+            if trie.find(2, word):
+                ret.append(word)
+        return ret
